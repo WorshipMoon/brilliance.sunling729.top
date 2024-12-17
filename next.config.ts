d3df513e2ac82f1052df4935fa5e68a13088d687
Moon <moon@163.com>
2024-12-17 18:36:06 +0800

    cssbug

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  /* 下面这段代码是为了解决打包压缩后，css样式不生效的问题 */
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (!dev) {
+      config.optimization.minimizer = [];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
