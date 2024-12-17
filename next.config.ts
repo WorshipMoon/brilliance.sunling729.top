f7d13e8ce0c899e7b5a7f0dc0be90363550f1a98
Moon <moon@163.com>
2024-12-17 18:52:47 +0800

    cssbug

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,7 @@ const nextConfig: NextConfig = {
   output: "standalone",
   /* 下面这段代码是为了解决打包压缩后，css样式不生效的问题 */
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    if (!dev) {
-      config.optimization.minimizer = [];
-    }
+    config.optimization.minimizer = [];
     return config;
   },
 };
